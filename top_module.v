a82bf99caeb8cc10c8d81de34caf419b614705f3
HemantArora2004 <HemantArora2004@gmail.com>
2024-02-08 21:07:57 -0500

    added uart_rx

--- top_module.v
+++ top_module.v
@@ -1,26 +1,13 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 02/08/2024 08:20:40 PM
-// Design Name: 
-// Module Name: top_module
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module top_module(
-
+    input clk,
+    input uart_tx_out,
+    output uart_rx_out,
+    output led
     );
+    
+      
 endmodule
+
+   
---
+++ uart_rx.v
@@ -0,0 +1,8 @@
+`timescale 1ns / 1ps
+
+module uart_rx(
+    input clk50Mhz_i,
+    input rst_i_n,
+    input rx_i
+    );
+endmodule
